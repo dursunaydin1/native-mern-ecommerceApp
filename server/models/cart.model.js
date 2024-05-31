76ac76feb9074a007d4c368f405662a5d3fb6728
dursunaydin1 <dursunayydin@gmail.com>
2024-05-31 14:54:48 +0300

    created models

---
+++ server/models/cart.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const cartModel = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    product: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Cart", cartModel);
