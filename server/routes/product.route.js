8963e7b12537a56b75aaebb8b9e36d3dc13cb79b
dursunaydin1 <dursunayydin@gmail.com>
2024-06-01 18:30:34 +0300

    CRUD product.controller

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,7 +1,18 @@
-const { createProduct } = require("../controllers/product.controller.js");
+const {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProduct,
+  getProducts,
+} = require("../controllers/product.controller.js");
 const { verifyAdmin } = require("../middleware/verifyToken.js");
 const { parser } = require("../utils/cloudinary.js");
 const router = require("express").Router();
 
 router.post("/", verifyAdmin, parser.single("image"), createProduct);
+router.put("/:id", verifyAdmin, parser.single("image"), updateProduct);
+router.delete("/:id", verifyAdmin, deleteProduct);
+router.get("/:id", getProduct);
+router.get("/", getProducts);
+
 module.exports = router;
