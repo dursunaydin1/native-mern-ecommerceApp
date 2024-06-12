935cbd9714816bb6f96ce4955504ac7e81925c2e
dursunaydin1 <dursunayydin@gmail.com>
2024-06-12 20:21:36 +0300

    Product Categories

---
+++ client/components/category/Categories.js
@@ -0,0 +1,60 @@
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { categoriesData } from "../../data/CategoriesData";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+
+const Categories = () => {
+  const navigation = useNavigation();
+  return (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      <View style={styles.container}>
+        {categoriesData?.map((item) => (
+          <View key={item._id}>
+            <TouchableOpacity
+              style={styles.catContainer}
+              onPress={() => navigation.navigate(item.path)}
+            >
+              <FontAwesome
+                name={item.icon}
+                size={18}
+                color="black"
+                style={styles.catIcon}
+              />
+              <Text style={styles.catText}>{item.name}</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Categories;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff",
+    padding: 5,
+    flexDirection: "row",
+  },
+  catContainer: {
+    padding: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  catIcon: {
+    fontSize: 30,
+    verticalAlign: "top",
+  },
+  catText: {
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+});
