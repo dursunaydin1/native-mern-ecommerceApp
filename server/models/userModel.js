b5ae400ac609d2a56f82771234f2ca4669f05f73
dursunaydin1 <dursunayydin@gmail.com>
2024-07-01 18:21:50 +0300

    Add User Shema Model

---
+++ server/models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: [true, "Please Enter Your Name"] },
+    email: {
+      type: String,
+      required: [true, "Please Enter Your Email"],
+      unique: [true, "Please Enter Unique Email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please Enter Your Password"],
+      minLength: [6, "Password should be greater than 6 characters"],
+    },
+    address: { type: String, required: [true, "Please Enter Your Address"] },
+    city: { type: String, required: [true, "Please Enter Your City"] },
+    country: { type: String, required: [true, "Please Enter Your Country"] },
+    phone: { type: String, required: [true, "Please Enter Your Phone"] },
+    profilePic: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("Users", userSchema);
